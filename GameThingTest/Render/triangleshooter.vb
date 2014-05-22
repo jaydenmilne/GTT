7017764c6d63952f7995434853fcdb24b2d2b465
jaydenmilne <roguenerd@gmail.com>
2014-05-21 19:29:29 -0500

    GTT 0.02
    New in GTT 0.02:
    - Faked momentum, baby
    - Menu still broken
    
    Originally Made - 12/1/2013

--- GameThingTest/Render/triangleshooter.vb
+++ GameThingTest/Render/triangleshooter.vb
@@ -51,22 +51,46 @@ Public Class TriangleShooter
         End If
 
         If Input.KeyStates(Keys.Up) Then
-            ThisVector += New System.Windows.Vector(Math.Cos(AngleInRad) * 5, Math.Sin(AngleInRad) * 5)
+            ThisVector += New System.Windows.Vector(Math.Cos(AngleInRad), Math.Sin(AngleInRad))
         End If
 
         If Input.KeyStates(Keys.Down) Then
-            CurrentAngle -= 1.0F / 2.0F * CSng(d)
+            ThisVector -= New System.Windows.Vector(Math.Cos(AngleInRad), Math.Sin(AngleInRad))
         End If
 
 
+        ' This is hopefully creating a copy of Unsized Geom
+        ' I don't actually know if it is, but I don't care,
+        ' because it works the way I expect it to and dosen't
+        ' deform due to floating point errors
 
         ThisPoints = UnsizedGeom.ToArray
 
-        TransMatrix.RotateAt(CurrentAngle, New PointF(CSng(LocationX + 50 + ThisVector.X), CSng(LocationY + 50 + ThisVector.Y)))
+        ' Increase location by the momentum vector
 
-        TransMatrix.Translate(CSng(ThisVector.X) + LocationX, CSng(ThisVector.Y + LocationY))
+        LocationX = CSng(LocationX + ThisVector.X)
+        LocationY = CSng(LocationY + ThisVector.Y)
+
+        ' Rotate by current angle, assuming that the figure is 100 px by 100 px
+        ' so 50 px is the center
+
+        TransMatrix.RotateAt(CurrentAngle, New PointF(CSng(LocationX + 50), CSng(LocationY + 50)))
+
+        ' Translate it 
+
+        TransMatrix.Translate(LocationX, LocationY)
+
+        ' Cause momentum vector to lose speed over time
+        ' This is a made up value
+
+        ThisVector.Y *= 0.99
+        ThisVector.X *= 0.99
+
+        ' Actually do the transformation on ThisPoints
 
         TransMatrix.TransformPoints(ThisPoints)
+
+        ' Finish update
 
         PublicGeometry = ThisPoints.ToArray
 
